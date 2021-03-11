edf6860fdb8a58ded7200a8854795f9f3be17f61
Aaron Presley <155617+AaronPresley@users.noreply.github.com>
2021-03-11 10:54:42 -0800

    GH Improvements: Actions + CODEOWNERS (#2)
    - This repo is now automatically running unit tests on each PR
    - Added a `CODEOWNERS` file
    - Established minimum test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   "preset": "ts-jest",
+  "collectCoverage": true,
+  "coverageThreshold": {
+    "global": {
+      "statements": 95,
+      "branches": 85,
+      "functions": 85,
+      "lines": 85,
+    }
+  }
 };
